7c7bd7bdd4bbc32bc9074d58d005d7fee88100b8
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2023-01-04 13:47:48 -0500

    convert to jsx fragmnet

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,8 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
+
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
@@ -37,7 +39,7 @@ const AddUser = (props) => {
     setError(null);
   };
   return (
-    <div>
+    <>
       {error && (
         <ErrorModal
           onConfirm={errorHandler}
@@ -64,7 +66,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </>
   );
 };
 
